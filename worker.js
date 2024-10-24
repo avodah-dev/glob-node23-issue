354c9c1b08acbcdaa3ac55b708a688ef533528f1
Nathan Bedford <nathan.bedford@gmail.com>
2024-10-23 22:28:26 -0400

    Added basic glob example

--- worker.js
+++ worker.js
@@ -1,7 +1,31 @@
-// worker.js
+const { glob } = require("glob");
 
-console.log("Worker started");
+console.log("Worker started\n");
 
+function testGlob(trailingSlash) {
+  const rootPath = "./static" + (trailingSlash ? "/" : "");
+
+  glob("**/**", {
+    cwd: rootPath,
+    absolute: false,
+    follow: true,
+    nodir: true,
+    dot: true,
+  })
+    .then((files) => {
+      console.log(`With cwd '${rootPath}:`, files);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function runTest() {
+  testGlob(true);
+  testGlob(false);
+}
+
+runTest();
 setInterval(() => {
-  console.log("Worker is doing some background work...");
+  console.log("Worker still alive...");
 }, 15000);
