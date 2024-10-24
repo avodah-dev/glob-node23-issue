8d03cfb17d105f2d984d2d7ad7ce6164a4970266
Nathan Bedford <nathan.bedford@gmail.com>
2024-10-23 22:20:26 -0400

    Added 'start' script

--- worker.js
+++ worker.js
@@ -4,5 +4,4 @@ console.log("Worker started");
 
 setInterval(() => {
   console.log("Worker is doing some background work...");
-  // Perform any background task here (e.g., checking a database, processing a queue, etc.)
-}, 5000); // This will run every 5 seconds
+}, 15000);
